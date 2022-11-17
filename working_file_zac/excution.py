addb6acf62cf344082362f318b7ca1feae3632c5
Zac Zhang <zac.z.zhang@outlook.com>
2022-11-17 10:48:27 +0100

    deleted:    dependencies/master_navigation_data.json 	new file:   fabrication/fabrication_and_visualization.ghx 	new file:   fabrication/master_navigation_data.json 	renamed:    dependencies/master_navigation_data.py -> fabrication/master_navigation_data.py 	modified:   working_file_zac/excution.py 	deleted:    working_file_zac/visualization.ghx 	new file:   working_file_zac/workflow.drawio 	new file:   working_file_zac/workflow.png 	new file:   working_file_zac/workflow.svg

--- working_file_zac/excution.py
+++ working_file_zac/excution.py
@@ -4,23 +4,16 @@ import simple_comm as c
 import json
 
 
-def UpdateNavigation():
-    with open(NAVIGATION_JSON) as f:
-        data = json.load(f)
-
-
-
-
-
-
-
 def set_robot_base():
-    pt_0 = rg.Point3d(data['origin'][0],data['origin'][1],data['origin'][2]) # base plane origin
-    pt_1 = rg.Point3d(data['origin_x'][0],data['origin_x'][1],data['origin_x'][2]) # point on positive x axis
-    pt_2 = rg.Point3d(data['origin_y'][0],data['origin_y'][1],data['origin_y'][2]) # point on positive xy plane
-    robot_base = rg.Plane(pt_0,pt_1-pt_0,pt_2-pt_0)
+    pt_0 = rg.Point3d(data["origin"][0], data["origin"][1], data["origin"][2])  # base plane origin
+    pt_1 = rg.Point3d(
+        data["origin_x"][0], data["origin_x"][1], data["origin_x"][2]
+    )  # point on positive x axis
+    pt_2 = rg.Point3d(
+        data["origin_y"][0], data["origin_y"][1], data["origin_y"][2]
+    )  # point on positive xy plane
+    robot_base = rg.Plane(pt_0, pt_1 - pt_0, pt_2 - pt_0)
     return robot_base
-
 
 
 def rhino_to_robot_space(rhino_plane):
@@ -29,32 +22,31 @@ def rhino_to_robot_space(rhino_plane):
     plane.Transform(rhino_matrix)
     return plane
 
-test_plane = debug_plane.Clone()
-
-
 
 def send(script):
     script = c.concatenate_script(script)
     c.send_script(script, ROBOT_IP)
     return script
 
+
+def tcp(script):
+    script += ur.set_tcp_by_angles(TCP[0], TCP[1], TCP[2], TCP[3], TCP[4], TCP[5])
+    return script
+
+
+# JSON
+def UpdateNavigation(script):
+    data = []
+    with open(NAVIGATION_JSON, "r") as f:
+        data = json.load(f)
+    TCP = data["tcp"]
+    ROBOT_IP = data["ip"]
+    ROBOT_BASE = set_robot_base()
+    return script 
+
+
 script = ""
-script = tcp(script)
-
-if is_debug_mode:
-    script += ur.move_l(rhino_to_robot_space(test_plane), SAFE_ROBOT_ACC, SAFE_ROBOT_VEL)
-
-
-a = ghdoc.Path
-
-path = a.split('3d_printing_teamwork')[0]+r'3d_printing_teamwork\\'+file_name
-
-
-
-ROBOT_IP = data['ip']
-TCP = data['tcp']
-ROBOT_BASE = set_robot_base()
-
-
-if fabricate:
+if send_debug_plane:
+    script = UpdateNavigation(script)
+    script += ur.move_l(rhino_to_robot_space(debug_plane), DEBUG_ROBOT_ACC, DEBUG_ROBOT_VEL)
     send(script)
