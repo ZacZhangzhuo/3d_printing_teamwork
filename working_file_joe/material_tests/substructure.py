750a54121e5a4fbad2e60b85558af327ff678830
Joseph Kenny <jckenny59@gmail.com>
2022-12-02 10:08:22 +0100

    editing_substructure committed

--- working_file_joe/material_tests/substructure.py
+++ working_file_joe/material_tests/substructure.py
@@ -27,23 +27,52 @@ for c in contours:
     cont_pts = []
     crv = c.ToNurbsCurve(2,5)
     params = crv.DivideByCount(pole_count, True)
-    
     for i in params:
         pt = crv.PointAt(i)
         cont_pts.append(pt)
     pts.append(cont_pts)
 
 poles = []
+pole_crvs = []
+
 
 for i in range(pole_count):
     temp = []
+    pole_circ = []
     for j, value in enumerate(pole_graph):
         pole_center_plane = rg.Plane(pts[j][i], rg.Vector3d.XAxis, rg.Vector3d.YAxis)
         pole_rad= rg.Circle(pole_center_plane, pole_rad_val*(0.5*pole_graph[j]))
         pole_arc = rg.ArcCurve(pole_rad)
         temp.append(pole_arc)
+        pole_circ.append(pole_rad)
     pole = rg.Brep.CreateFromLoft(temp, rg.Point3d.Unset,rg.Point3d.Unset,rg.LoftType(1),False)
+    pole_crvs.append(pole_circ)
     poles.append(pole)
+
+edge_pts = []
+
+for list in pole_crvs:
+    circ_pts = []
+    for arc in list:
+        crv = arc.ToNurbsCurve(2,5)
+        params = crv.DivideByCount(4, True)
+        for i in params:
+            pt = crv.PointAt(i)
+            circ_pts.append(pt)
+        edge_pts.append(circ_pts)
+
+
+pole_loft = []
+
+for list in edge_pts:
+    vert_crvs = []
+    for k in list:
+        index_line = rg.Curve.CreateInterpolatedCurve(k[i], 3)
+        vert_crvs.append(index_line)
+    srf = rg.Brep.CreateFromLoft(vert_crvs, rg.Point3d.Unset,rg.Point3d.Unset,rg.LoftType(1),False)
+    pole_loft.append(srf)
+    
+
 
 # # str_planes = []
 
