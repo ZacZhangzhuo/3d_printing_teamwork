8f6782b849ffd40c3c32160a220f391f64f11721
Eleni Alexi <elenialex96@gmail.com>
2022-12-06 15:05:25 +0100

    Updating Planes_send multiple paths

--- PlaneFabrication/01_planes_send.py
+++ PlaneFabrication/01_planes_send.py
@@ -42,18 +42,22 @@ print("Print data loaded :", os.path.join(DATA_OUTPUT_FOLDER, PRINT_FILE_NAME))
 # Define print data containers as empty lists
 planes = json_load(r"PlaneFabrication\data\output\out_printpoints.json")
 frames = []
-for p in planes:
-    frames.append(Frame.from_plane(p))
+for lists in planes:
+    for list in lists:
+        small_list=[]
+        for p in list:
+            print (p)
+            small_list.append(Frame.from_plane(p))
+    frames.append(small_list)
 
 
-velocities = [100] *len(frames)
-radii = [0] * len(frames)
-toggles = []
-for t in range(len(frames)):
-    if t % 11 == 0:
-        toggles.append(1)
-    else:
-        toggles.append(0)
+velocities = [[20] *len(frames[0])]* len(frames)
+radii = [[5] * len(frames[0])]* len(frames)
+toggles = [[0] * len(frames[0])]* len(frames)
+
+for list in toggles:
+    list[-1]=2
+
 #toggles = [0,0,0,2,0,0,0,0]
 #toggles = [True]*(len(frames))
 # Go through JSON and copy data to lists
@@ -62,6 +66,7 @@ for t in range(len(frames)):
 # point = data[item]['frame']['point']
 # xaxis = data[item]['frame']['xaxis']
 # yaxis = data[item]['frame']['yaxis']
+
 # Fill containers for velocities
 
 #     frames.append(frame)
@@ -73,6 +78,7 @@ for t in range(len(frames)):
 
 # Use the data to execute the printpath
 if __name__ == "__main__":
+    print (velocities, toggles, radii)
     # Create base frame with measured points
     ORIGIN = Point(origin[0], origin[1], origin[2])
     XAXIS_PT = Point(origin_x[0], origin_x[1], origin_x[2])
@@ -81,14 +87,18 @@ if __name__ == "__main__":
     base_frame = Frame.from_points(ORIGIN, XAXIS_PT, YAXIS_PT)
     # base_frame = Frame(Point(548.032, 552.647, -2.884), Vector(-1.000, -0.013, 0.002), Vector(0.013, -1.000, 0.003))
 
-    # Transform all frames from slicing location to robot coordinate system
-    T = Transformation.from_frame_to_frame(Frame.worldXY(), base_frame)
-    frames = [f.transformed(T) for f in frames]
+    i=0
+    for frame_list in frames:
 
-    # Move frames by a Z fine tuning parameter as required
-    T = Translation.from_vector([0, 0, Z_OFFSET])
-    frames = [f.transformed(T) for f in frames]
+        # Transform all frames from slicing location to robot coordinate system
+        T = Transformation.from_frame_to_frame(Frame.worldXY(), base_frame)
+        frame_list = [f.transformed(T) for f in frame_list]
 
-    # IP_ADDRESS = "127.0.0.1"
-    # Send all points using send_printpath function implemented in the RTDE Wrapper
-    rtde.send_printpath(frames, velocities, MAX_ACCEL, radii, toggles, ip=IP_ADDRESS)
+        # Move frames by a Z fine tuning parameter as required
+        T = Translation.from_vector([0, 0, Z_OFFSET])
+        frame_list = [f.transformed(T) for f in frame_list]
+
+        # IP_ADDRESS = "127.0.0.1"
+        # Send all points using send_printpath function implemented in the RTDE Wrapper
+        rtde.send_printpath(frame_list, velocities[i], MAX_ACCEL, radii[i], toggles[i], ip=IP_ADDRESS)
+        i+=1
