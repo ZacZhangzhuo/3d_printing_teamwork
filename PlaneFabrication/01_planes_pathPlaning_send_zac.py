9fe04783d94da415aa6a1f4e1b0b384a8d38e04f
Zac Zhang <zac.z.zhang@outlook.com>
2022-12-07 19:10:01 +0100

    Add

--- PlaneFabrication/01_planes_pathPlaning_send_zac.py
+++ PlaneFabrication/01_planes_pathPlaning_send_zac.py
@@ -56,54 +56,13 @@ for i in range(len(configs)):
         ]
     )
     # print(c)
+
+
+
+
+ur_c = RTDEControl(IP_ADDRESS)
+ur_c.moveJ([0.873, -1.222, -2.793, 0.262, 1.571, -3.142], 0.8, 0.5, 0) #! Configuration_2
 rtde.send_configs(c, 0.3, 1.0,  0.001, toggles= False , ip = IP_ADDRESS)
 
 
-# ur_c = RTDEControl(IP_ADDRESS)
-# ur_c.moveJ([0.873, -1.222, -2.793, 0.262, 1.571, -3.142], 0.8, 0.5, 0) #! Configuration_2
 
-
-# toggles = []
-# for t in range(len(frames)):
-#     if t % 11 == 0:
-#         toggles.append(1)
-#     else:
-#         toggles.append(0)
-# #toggles = [0,0,0,2,0,0,0,0]
-# #toggles = [True]*(len(frames))
-# # Go through JSON and copy data to lists
-# # for item in data:
-# # Read frame data
-# # point = data[item]['frame']['point']
-# # xaxis = data[item]['frame']['xaxis']
-# # yaxis = data[item]['frame']['yaxis']
-# # Fill containers for velocities
-
-# #     frames.append(frame)
-# #     velocities.append(data[item]['velocity'])
-# #     radii.append(data[item]['blend_radius'])
-# #     toggles.append(data[item]['extruder_toggle'])
-# # #######################################################################
-
-
-# # Use the data to execute the printpath
-# if __name__ == "__main__":
-#     # Create base frame with measured points
-#     ORIGIN = Point(origin[0], origin[1], origin[2])
-#     XAXIS_PT = Point(origin_x[0], origin_x[1], origin_x[2])
-#     YAXIS_PT = Point(origin_y[0], origin_y[1], origin_y[2])
-#     # Create a compas Frame
-#     base_frame = Frame.from_points(ORIGIN, XAXIS_PT, YAXIS_PT)
-#     # base_frame = Frame(Point(548.032, 552.647, -2.884), Vector(-1.000, -0.013, 0.002), Vector(0.013, -1.000, 0.003))
-
-#     # Transform all frames from slicing location to robot coordinate system
-#     T = Transformation.from_frame_to_frame(Frame.worldXY(), base_frame)
-#     frames = [f.transformed(T) for f in frames]
-
-#     # Move frames by a Z fine tuning parameter as required
-#     T = Translation.from_vector([0, 0, Z_OFFSET])
-#     frames = [f.transformed(T) for f in frames]
-
-#     # IP_ADDRESS = "127.0.0.1"
-#     # Send all points using send_printpath function implemented in the RTDE Wrapper
-#     rtde.send_printpath(frames, velocities, MAX_ACCEL, radii, toggles, ip=IP_ADDRESS)
